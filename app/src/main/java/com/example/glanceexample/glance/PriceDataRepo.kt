a218194991a5234aa63db4a43eccd91f44bb6ef7
Admin <vadimsuman55@gmail.com>
2025-11-30 16:44:26 +0700

    PriceDataRepo

---
+++ app/src/main/java/com/example/glanceexample/glance/PriceDataRepo.kt
@@ -0,0 +1,15 @@
+package com.example.glanceexample.glance
+
+object PriceDataRepo {
+    var ticker = "GOOGL"
+    private var previousPrice = 0f
+    var change = 0
+    private var _currentPrice = MutableStateFlow(0f)
+    val currentPrice: StateFlow<Float> get() = _currentPrice
+    fun update() {
+        previousPrice = currentPrice.value
+        _currentPrice.value = Random.nextInt(20, 35) + Random.nextFloat()
+        change = ((_currentPrice.value - previousPrice)
+                / previousPrice * 100).toInt()
+    }
+}
