72ca8e9151c97d47ef718b68f861041e2e1ae597
Admin <vadimsuman55@gmail.com>
2025-11-30 18:03:35 +0700

    Добавление поддержки режима изменения размера

--- app/src/main/java/com/example/glanceexample/glance/StockAppWidget.kt
+++ app/src/main/java/com/example/glanceexample/glance/StockAppWidget.kt
@@ -4,13 +4,16 @@ import android.content.Context
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.unit.DpSize
 import androidx.compose.ui.unit.dp
 import androidx.glance.GlanceId
 import androidx.glance.GlanceModifier
 import androidx.glance.GlanceTheme
+import androidx.glance.ImageProvider
 import androidx.glance.appwidget.GlanceAppWidget
 import androidx.glance.appwidget.provideContent
 import androidx.glance.background
+import androidx.glance.layout.Alignment
 import androidx.glance.layout.Column
 import androidx.glance.layout.fillMaxSize
 import androidx.glance.layout.padding
@@ -24,10 +27,23 @@ import kotlinx.coroutines.launch
 import java.time.Duration
 import java.util.Locale
 import androidx.compose.ui.unit.sp
-
+import androidx.glance.Image
+import androidx.glance.LocalSize
+import androidx.glance.appwidget.SizeMode
+import androidx.glance.appwidget.cornerRadius
+import com.example.glanceexample.R
 
 class StockAppWidget : GlanceAppWidget() {
     private var job: Job? = null
+
+    companion object {
+        private val smallMode = DpSize(100.dp, 80.dp)
+        private val mediumMode = DpSize(120.dp, 120.dp)
+    }
+
+    override val sizeMode: SizeMode = SizeMode.Responsive(
+        setOf(smallMode, mediumMode)
+    )
 
     override suspend fun provideGlance(context: Context, id: GlanceId) {
         if (job == null) {
@@ -53,7 +69,11 @@ class StockAppWidget : GlanceAppWidget() {
     @Composable
     fun GlanceContent() {
         val stateCount by PriceDataRepo.currentPrice.collectAsState()
-        Small(stateCount)
+        val size = LocalSize.current
+        when {
+            size.width >= mediumMode.width && size.height >= mediumMode.height -> Medium(stateCount)
+            else -> Small(stateCount)
+        }
     }
 
     @Composable
@@ -97,6 +117,32 @@ class StockAppWidget : GlanceAppWidget() {
                 .padding(8.dp)
         ) {
             StockDisplay(stateCount)
+        }
+    }
+
+    @Composable
+    private fun Medium(stateCount: Float) {
+        Column(
+            horizontalAlignment = Alignment.Horizontal.CenterHorizontally,
+            modifier = GlanceModifier
+                .fillMaxSize()
+                .cornerRadius(15.dp)
+                .background(GlanceTheme.colors.background)
+                .padding(8.dp)
+        ) {
+            StockDisplay(stateCount)
+            Image(
+                provider = ImageProvider(
+                    if (PriceDataRepo.change > 0)
+                        R.drawable.up_arrow
+                    else
+                        R.drawable.down_arrow
+                ),
+                contentDescription = "Arrow Image",
+                modifier = GlanceModifier
+                    .fillMaxSize()
+                    .padding(20.dp)
+            )
         }
     }
 }
