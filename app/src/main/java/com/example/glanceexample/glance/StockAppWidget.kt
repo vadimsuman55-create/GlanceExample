0355a5ea59247006a0aac4756c033f07f5af53e4
Admin <vadimsuman55@gmail.com>
2025-11-30 17:27:37 +0700

    Моделирование изменения цен

--- app/src/main/java/com/example/glanceexample/glance/StockAppWidget.kt
+++ app/src/main/java/com/example/glanceexample/glance/StockAppWidget.kt
@@ -13,14 +13,34 @@ import androidx.glance.background
 import androidx.glance.layout.Column
 import androidx.glance.layout.fillMaxSize
 import androidx.glance.layout.padding
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.launch
+import java.time.Duration
 
 class StockAppWidget : GlanceAppWidget() {
+    private var job: Job? = null
 
     override suspend fun provideGlance(context: Context, id: GlanceId) {
+        if (job == null) {
+            job = startUpdateJob(Duration.ofSeconds(20).toMillis(), context)
+        }
 
         provideContent {
             GlanceTheme {
                 GlanceContent()
+            }
+        }
+    }
+
+    private fun startUpdateJob(timeInterval: Long, context: Context): Job {
+        return CoroutineScope(Dispatchers.Default).launch {
+            while (true) {
+                PriceDataRepo.update()
+
+                delay(timeInterval)
             }
         }
     }
